2da438ae3c42f3debdee36630a569087d5196d87
shubham <ito.k23018@gmail.com>
2024-06-15 11:27:56 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="grid place-items-center min-h-screen">
+      <div className="bg-yellow-200 rounded-full aspect-square w-[300px] relative overflow-hidden">
+        <section className="bg-red-300 absolute h-[100px] w-[80px]">
+          asdasd
+        </section>
+        <section>1212</section>
+        <section>zxc</section>
+        <section>asd</section>
+        <section>lklk</section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
